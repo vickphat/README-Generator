9f94053c6b1d0ec149242453dd3ea2fa7d55f519
Vicheka Phat <vickphat@gmail.com>
2021-04-22 23:32:03 -0400

    added function to create README and fs.writeFile for prompt data

---
+++ README.md
@@ -0,0 +1,34 @@
+# tshgfdh
+
+# Table of Contents 
+
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [Contribution](#contribution)
+5. [License](#license)
+6. [Questions](#questions)
+    
+## Description: 
+fdghfgh
+
+## Installation: 
+fghfgdhdfgh
+
+## Usage: 
+fgdhfgdhfdgh
+
+## License: 
+MIT License
+
+## Contribution: 
+undefined
+
+## Testing: 
+gfdfigjd
+
+## Questions: 
+If there are any questions that you may have, please contact me by the following:
+
+    * Github: [sdoifhsd](https://github.com/sdoifhsd)
+    * Email: ;s'dzoghidfsg 
--- index.js
+++ index.js
@@ -11,42 +11,88 @@ inquirer
         {
             type: 'input',
             name: 'description',
-            message:'Write a brief summary of your project',    
+            message: 'Write a brief summary of your project',
         },
         {
             type: 'input',
             name: 'installation',
-            message:'What are the steps required to install your project?', 
+            message: 'What are the steps required to install your project?',
         },
         {
             type: 'input',
             name: 'usage',
-            message:'What is your project used for?',    
+            message: 'What is your project used for?',
         },
         {
             type: 'input',
-            name: 'collaborators',
-            message:'Were there any collaborators for this project?',    
+            name: 'contribution',
+            message: 'Were there any collaborators for this project?',
         },
         {
             type: 'input',
             name: 'test',
-            message:'How did you test your project?',    
+            message: 'How did you test your project?',
         },
         {
             type: 'checkbox',
             name: 'license',
-            message:'Is this project licensed?',
+            message: 'Is this project licensed?',
             choices: ['MIT License', 'Apache License 2.0', 'GNU AGPLv3', 'The Unlicense'],
         },
         {
             type: 'input',
             name: 'github',
-            message:'What is your GitHub username?',    
+            message: 'What is your GitHub username?',
         },
         {
             type: 'input',
             name: 'email',
-            message:'What is your email?',    
+            message: 'What is your email?',
         },
     ])
+    .then((data) => {
+        const readMeFile = readMe(data);
+
+        fs.writeFile('README.md', readMeFile, (err) =>
+            err ? console.log(err) : console.log('Successfully created README file!')
+        );
+    });
+
+var readMe = data => {
+    return `# ${data.title}
+
+# Table of Contents 
+
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [Contribution](#contribution)
+5. [License](#license)
+6. [Questions](#questions)
+    
+## Description: 
+${data.description}
+
+## Installation: 
+${data.installation}
+
+## Usage: 
+${data.usage}
+
+## License: 
+${data.license}
+
+## Contribution: 
+${data.collaborators}
+
+## Testing: 
+${data.test}
+
+## Questions: 
+If there are any questions that you may have, please contact me by the following:
+
+    * Github: [${data.github}](https://github.com/${data.github})
+    * Email: ${data.email} `;
+    
+}
+ 
